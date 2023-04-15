bd91937dc1615945909ff42934450d47cf06489b
elty-annie <eltinangwarai@gmail.com>
2023-04-15 23:05:23 +0200

    fixed  a typo

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default function About(){
             <img className="portrait" src={require('../images/eltie.jpg')} alt="aboutme-pic" />
             <div className="bio">
                 <h1>About me</h1>
-                <p>Coming from a background in accounting, I had previously neven seen myself pursuing a career in Software Development. Then I discovered SheCodes! Now, less than a year later I have spent hundreds of hours learning frontend development with React and i love it. I have built multiple projects along the way and will only be adding more to that list.</p>
+                <p>Coming from a background in accounting, I had previously never seen myself pursuing a career in Software Development. Then I discovered SheCodes! Now, less than a year later I have spent hundreds of hours learning frontend development with React and i love it. I have built multiple projects along the way and will only be adding more to that list.</p>
             </div>
         </div>
     );
