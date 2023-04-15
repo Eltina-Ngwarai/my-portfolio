7bbf0d81d2b80ad2999975699de6061c441d6f58
elty-annie <eltinangwarai@gmail.com>
2023-04-16 01:11:16 +0200

    made the navbar sticky

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,12 @@ import "../styles/Navbar.css"
 
 export default function Navbar(){
     return (
-    <div className="navbar">
-        <div className="toggleBar"></div>
-
+    <div className="navigation-bar">
         <div className="links">
             <ul>
                 <li><a href="/">HOME</a></li>
                 <li><a href="/#About">ABOUT</a></li>
                 <li><a href="/#Projects">PROJECTS</a></li>
-                <li><a href="/#Contact">CONTACT</a></li>
             </ul>
         </div>
     </div>
