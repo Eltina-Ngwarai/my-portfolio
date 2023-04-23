77805cba7231a1e208d1d8672b600b24dfafee9b
elty-annie <eltinangwarai@gmail.com>
2023-04-23 21:47:29 +0200

    updated cv

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ export default function About(){
             <div className="bio">
                 <h1>About me</h1>
                 <p>Coming from a background in accounting, I had previously never seen myself pursuing a career in Software Development. Then I discovered SheCodes! Now, less than a year later I have spent hundreds of hours learning frontend development with React and i love it. I have built multiple projects along the way and will only be adding more to that list.</p>
-            <a href="https://drive.google.com/file/d/1_YrYamnCLXDixQkPg_IMlhinSLT9uuyH/view?usp=sharing" className="resumelink" target="_blank" rel="noreferrer">My Resume</a>
+            <a href="https://drive.google.com/file/d/1BZEsTan8oC7io2JMXQfB_QhCFzriHhgj/view?usp=sharing" className="resumelink" target="_blank" rel="noreferrer">My Resume</a>
             </div>
         </div>
     );
